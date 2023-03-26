97b3d61eae52602084a3ec8e65784db50ea56412
DrinkerTea <mitrofankin@gmail.com>
2023-03-26 12:27:25 +0600

    new

--- js/numcount.js
+++ js/numcount.js
@@ -8,10 +8,29 @@ counters.forEach((counter) => {
         const c = +counter.innerText;
 
         if (c < target) {
-            counter.innerText = c + 1;
+            counter.innerText = c + 5;
             setTimeout(updateCounter, 1);
         } else{
             counter.innerText = target;
+        }
+    };
+    updateCounter()
+})
+
+const counters1 = document.querySelectorAll(".counter1");
+
+counters1.forEach((counter1) => {
+    counter1.innerHTML = "0";
+
+    const updateCounter = () => {
+        const target = +counter1.getAttribute("data-target");
+        const c = +counter1.innerText;
+
+        if (c < target) {
+            counter1.innerText = c + 1;
+            setTimeout(updateCounter, 1);
+        } else{
+            counter1.innerText = target;
         }
     };
     updateCounter()
